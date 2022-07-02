4583a5681ea74c04ecc30479bfdd7e74632f24d8
Stephanie Sadowski <stephaniesadowski@Stephanies-Air.home>
2022-07-01 23:04:56 -0400

    created frontend api with css, scripts and html

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// create JSON file in local API with all restuarants
+// fetch local API and render restaurant list here
+
+const restaurantList = document.querySelector('.restaurant-data')
+
+fetch('http://localhost:4002/')
+    .then(response => response.json())
+    .then(restaurantList => {
+        let html = ''
+        restaurantList.forEach(restaurant => {
+            let htmlData = 
+            `<div class="restaurant-data"></div>
+            <img src="${restaurant.photoUrl}">
+            <h3>${restaurant.name}</h3>
+            <h4>Rating: ${restaurant.rating}/h4>
+            <p>${restaurant.address}</p>`
+
+        html += htmlData
+        });
+        restaurantList.innerHTML = html
+    })
+
+    .catch(error => console.error(error))
